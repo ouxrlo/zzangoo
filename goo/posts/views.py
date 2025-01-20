faea13f6fb5cf7f72db577f8bded03be2b9f899a
ouxrlo <ouxrlo3@gmail.com>
2025-01-21 06:24:15 +0900

    Add Post_List

--- goo/posts/views.py
+++ goo/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    post = Post.objects.all()
+    paginator = Paginator(post, 10)
+
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, "posts/post_list.html", {"page_obj": page_obj})
